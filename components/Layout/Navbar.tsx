23e74d618bcaa1f66e3dea5e077d4702bfe95ca0
Philani Ncube <ncbphi001@gmail.com>
2022-09-20 19:06:44 +0200

    menu update

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,8 +1,13 @@
+import {useState} from "react"
 import Link from "next/link";
 import {motion, AnimatePresence} from 'framer-motion'
 import { useRouter } from "next/router";
+import Image from "next/future/image";
+import { HiMenu } from "react-icons/hi";
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
 
   const router = useRouter()
 
@@ -27,12 +32,18 @@ const Navbar = () => {
 
 
   return (
-    <header className="absolute z-20 bg-transparent top-0 left-0 right-0">
+    <header className="z-20 bg-white py-2">
       {/**Desktop Navigation Starts**/}
       <nav className="hidden md:flex max-w-7xl mx-auto px-4 justify-between items-center">
-        <h3 className="text-2xl font-light font-sans text-gray-700 uppercase">
-          Mudita
-        </h3>
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            width={566.9}
+            height={289.9}
+            alt="logo"
+            className="w-44 object-cover"
+          />
+        </Link>
 
         <div className="flex items-center">
           <ul className="flex items-center  h-full">
@@ -41,7 +52,7 @@ const Navbar = () => {
                 layout
                 key={item.title}
                 className={`h-full transition-all duration-500 py-4 px-2 cursor-pointer ${
-                  item.active ? "bg-slate-200" : ""
+                  item.active ? "" : ""
                 }`}
               >
                 <Link href={item.href}>
@@ -58,11 +69,60 @@ const Navbar = () => {
               </motion.li>
             ))}
           </ul>
-
         </div>
       </nav>
 
       {/**Desktop Navigation Ends**/}
+      <nav className="container flex md:hidden px-4 items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            width={566.9}
+            height={289.9}
+            alt="logo"
+            className="w-32 object-cover"
+          />
+        </Link>
+
+        <HiMenu
+          onClick={() => setOpen(true)}
+          className="h-12 w-12 text-gray-600"
+        />
+        <AnimatePresence exitBeforeEnter initial={false}>
+          {open && (
+            <motion.div
+              className="w-full z-30 h-screen items-center list-none bg-slate-700 p-8 flex flex-col justify-around absolute inset-0"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+              onClick={() => setOpen(false)}
+            >
+              {navLinks.map((item) => (
+                <motion.li
+                  layout
+                  key={item.title}
+                  className={`h-full transition-all duration-500 py-4 px-2 cursor-pointer ${
+                    item.active ? "" : ""
+                  }`}
+                >
+                  <Link href={item.href}>
+                    <motion.a
+                      layout
+                      transition={{ duration: 0.3 }}
+                      className={`uppercase font-medium transition-all duration-500 ${
+                        item.active ? "text-gray-50" : "text-gray-100"
+                      }`}
+                    >
+                      {item.title}
+                    </motion.a>
+                  </Link>
+                </motion.li>
+              ))}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </nav>
 
       {/**Mobile Navigation Starts**/}
 
