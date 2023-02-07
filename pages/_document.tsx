c33e18d9468a2d6756b670480d29d20b34726f76
Philani Ncube <ncbphi001@gmail.com>
2023-02-07 23:02:59 +0200

    _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="use-credentials"
           />
          <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:wght@400;600;700&display=swap" rel="stylesheet"/>
 
