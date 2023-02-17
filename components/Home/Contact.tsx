c8f85c76ae34613163a08fe5ba9891aa614d7c95
Philani Ncube <ncbphi001@gmail.com>
2023-02-17 20:53:20 +0200

    whatsapp and instagram

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import React from "react";
-import { HiMail, HiMap, HiPhone } from "react-icons/hi";
+import { HiMail, HiPhone } from "react-icons/hi";
+import { ImInstagram, ImWhatsapp } from "react-icons/im";
 
 export default function Contact() {
   return (
@@ -10,14 +12,15 @@ export default function Contact() {
             Contact us
           </h1>
 
-          <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-2  mt-10 gap-6">
+          <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  mt-10 gap-6">
             <div className="bg-slate-100 border rounded-md border-gray-200 h-64 flex flex-col items-center">
               <div
-                className="w-16 mt-8 h-16 bg-indigo-50 rounded-full flex items-center justify-center"
+                className=" mt-8  bg-indigo-50 rounded-full flex items-center justify-center"
                 role="img"
                 aria-label="phone number"
               >
                 <HiPhone className="h-12 w-12" />
+                <ImWhatsapp className="h-12 w-12" />
               </div>
               <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">
                 Phone
@@ -42,6 +45,22 @@ export default function Contact() {
               </p>
               <p className="text-base mt-4 leading-4 text-center text-gray-800">
                 info@muditafoundationsa.co.za
+              </p>
+            </div>
+            <div className="bg-slate-100 border rounded-md border-gray-200 h-64 flex flex-col items-center ">
+              <Link
+                href="https://instagram.com/muditafoundation_sa?igshid=YmMyMTA2M2Y="
+                className="w-16 mt-8 h-16 bg-indigo-50 rounded-full flex items-center justify-center "
+                role="img"
+                aria-label="instagram"
+              >
+                <ImInstagram className="h-12 w-12" />
+              </Link>
+              <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">
+                Instagram
+              </p>
+              <p className="text-base mt-4 leading-4 text-center text-gray-800">
+                muditafoundation_sa
               </p>
             </div>
           </div>
